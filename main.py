1245a426fd8b654e2450d0141ac3693af6651c7b
kazbekovetolkunai10-sketch <kazbekovatolkunai10@gmail.com>
2026-01-28 16:31:21 +0600

    done

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+import predict
+
+house_app = FastAPI()
+house_app.include_router(predict.predict_router)
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(house_app, host='127.0.0.1', port=8000)
